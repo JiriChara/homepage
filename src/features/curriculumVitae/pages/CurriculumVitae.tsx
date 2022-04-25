00ba67d001fdce8d003780953404f44e129d31b3
Jiri Chara <jiri@chara.vip>
2022-04-25 23:48:25 +0200

    Finalize CV

--- src/features/curriculumVitae/pages/CurriculumVitae.tsx
+++ src/features/curriculumVitae/pages/CurriculumVitae.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 
+import CvSection from '../components/CvSection';
 import CvIntro from '../components/CvIntro';
 import CvEmploymentHistory from '../components/CvEmploymentHistory';
 import CvEducation from '../components/CvEducation';
@@ -24,6 +25,27 @@ export default function CurriculumVitae() {
       <CvSkills />
 
       <CvLanguages />
+
+      <CvSection title="Driving">
+        <p>
+          Holder of European driving license, Category B
+        </p>
+      </CvSection>
+
+      <CvSection title="Hobbies & Interests">
+        <p>
+          Music, traveling, hiking, cooking, drones, technology, programming, running, tea, spicy
+          food, reading, etc.
+        </p>
+      </CvSection>
+
+      <CvSection title="Professional Interests">
+        <p>
+          Web Development, Single-Page Applications, Vue.js, Progressive Web Applications, Front-End
+          Architecture, Node.js, Mentoring, Front-End Performance, Test-Driven Development, Agile
+          Ways of Working, Get Things Done!
+        </p>
+      </CvSection>
     </article>
   );
 }
