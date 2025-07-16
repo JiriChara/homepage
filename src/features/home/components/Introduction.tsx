32dc6c76bf48087c3eac5739282db3a3e41a8101
Jiri Chara <jiri.chara@pm.me>
2025-07-16 08:13:59 +0200

    Improve introduction clarity

--- src/features/home/components/Introduction.tsx
+++ src/features/home/components/Introduction.tsx
@@ -4,23 +4,24 @@ export default function Introduction() {
   return (
     <section className="container mx-auto">
       <p className="my-4">
-        Welcome visitor!
+        Welcome, visitor!
       </p>
 
       <p className="mb-4">
-        My name is Jiří Chára and I currently work as a senior software engineer at
+        My name is Jiří Chára, and I currently work as a senior software engineer at
         { ' ' }
         <HpeLink />
         . I live in Munich, but I can call many
-        places home. I love traveling, music, spicy food, running, coconuts, Thailand, the sun,
-        drones, and making long comma-separated lists.
+        places home. I love traveling, music, spicy food, running, coconuts, the sun,
+        drones—and making long comma-separated lists.
       </p>
 
       <p>
         I am passionate about building excellent software that improves the lives of those around
         me. I specialize in creating software for clients ranging from small businesses to large
-        enterprise corporations. I like to build and contribute to open source to help other
-        developers become productive and more successful.
+        enterprise corporations. I enjoy building and contributing to open-source projects to help
+        other developers become more productive and successful.
+
       </p>
     </section>
   );
